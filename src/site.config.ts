995b60124217df6d24f3b084af14fb2c40f23a84
chitori <so487193026@foxmail.com>
2026-09-13 21:10:44 +0800

    chore: rename site title to chitori.blog

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'chitori.fun',
+  title: 'chitori.blog',
   /** Will be used in index page & copyright declaration */
   author: 'chitori',
   /** Description metadata for your website. Can be used in page metadata. */
